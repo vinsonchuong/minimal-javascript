327316cc32eaaa1bf18ebbd90e0169d8281dce2f
Vinson Chuong <vinsonchuong@gmail.com>
2016-04-05 13:19:25 -0700

    factorsOf(3)

--- index.js
+++ index.js
@@ -30,11 +30,12 @@ function assertEqual(actual, expected) {
 
 function factorsOf(number) {
   const factors = []
-  if (number === 2) {
-    factors.push(2)
+  if (number > 1) {
+    factors.push(number)
   }
   return factors
 }
 
 assertEqual(factorsOf(1), [])
 assertEqual(factorsOf(2), [2])
+assertEqual(factorsOf(3), [3])
